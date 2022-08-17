7c1d8bad63001d999622f76284bb98b4d64cf20c
Harshal Khandalkar <Harshal Khandalkar@ASUSROG>
2022-08-17 17:39:21 +0530

    16-08-2022

---
+++ Assignment_16_08_2022/src/org/Assignment/Demo.java
@@ -0,0 +1,46 @@
+package org.Assignment;
+import java.util.Scanner;
+
+public class Demo {
+	
+	void start() {
+		System.out.println("Start");	
+	}
+	
+	void displayInstruction() {
+		System.out.println("DisplayInstruction");
+	}
+	
+	void exit() {
+		System.out.println("exit");
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Type number : ");
+		
+		
+		Scanner sc = new Scanner(System.in);
+		int mainInput = sc.nextInt();
+		
+		Demo d1 = new Demo();
+		
+		switch(mainInput) {
+		
+		case 1:
+				d1.start();
+				break;
+			
+		case 2:
+				d1.displayInstruction();
+				break;
+			
+		case 3:
+				d1.exit();
+				break;
+		
+		default :
+				System.out.println("Invalid Number");
+		}
+	}
+}
