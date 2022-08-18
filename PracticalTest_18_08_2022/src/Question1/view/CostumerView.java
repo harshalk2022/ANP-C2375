77841cf7e37f2c9e473d4c9dbaa1f59970f6123d
Harshal Khandalkar <Harshal Khandalkar@ASUSROG>
2022-08-18 17:20:44 +0530

    2nd commit

--- PracticalTest_18_08_2022/src/Question1/view/CostumerView.java
+++ PracticalTest_18_08_2022/src/Question1/view/CostumerView.java
@@ -5,10 +5,11 @@ import Question1.service.CostumerService;
 
 public class CostumerView {
 	public static void main(String[] args) {
-		//Costumer costumer1 = new Costumer(3, "Harshal", 34); 
+		//Costumer costumer1 = new Costumer(3, "Harshal", 34); //For calling paramiterize constructor
 		
-		Costumer costumer1=new Costumer();
-		costumer1.setCostumerage(2);
+		Costumer costumer1=new Costumer(); // for calling empty constructor
+		costumer1.setCostumerage(56);
+		
 		CostumerService isEligible = new CostumerService();
 		boolean criateria = isEligible.checkEligiblity(costumer1);
 		
